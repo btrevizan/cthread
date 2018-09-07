8871cf7a7372c7e020139919d06ad4b0a0c56e23
Gabriel Haggstrom <gshaggstrom@gmail.com>
2018-09-07 15:24:50 -0300

    Adiciona testes para csem_init.

---
+++ teste/csem_init_test.c
@@ -0,0 +1,18 @@
+#include "../include/cthread.h"
+#include "teste.h"
+
+int main() {
+    csem_t semaforo = {0, NULL};
+    int ret_code;
+
+    start_test("csem_init");
+
+    ret_code = csem_init(&semaforo, 3);
+
+    assert("Retorna 0", ret_code == 0);
+
+    assert("O contador deve iniciar com o valor fornecido",
+           semaforo.count == 3);
+
+    assert("A fila inicia vazia", semaforo.fila->first == NULL);
+}
