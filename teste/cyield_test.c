8169db8b0b8e3c4f54cb126f636d14bb80a33788
Gabriel Haggstrom <gshaggstrom@gmail.com>
2018-09-07 22:35:00 -0300

    Aprimora os testes de cyield para testar se o escalonador respeita a politica FIFO.

--- teste/cyield_test.c
+++ teste/cyield_test.c
@@ -2,63 +2,78 @@
 #include "teste.h"
 #include <string.h>
 
-void* func_2(void* trace) {
-    strcat((char*) trace, "2");
+typedef struct args_t {
+    char* trace;
+    char number_str[2];
+    int prio;
+} args_t;
+
+void* func(void* arg) {
+    args_t* args = (args_t *) arg;
+
+    strcat(args->trace, args->number_str);
     cyield();
 
     return NULL;
 }
 
-void* func_3(void* trace) {
+void* func2(void* trace) {
     strcat((char*) trace, "3");
-    cyield();
-
-    return NULL;
-}
-
-void* func_4(void* trace) {
-    strcat((char*) trace, "4");
-    cyield();
-
-    return NULL;
-}
-
-void* high_prio(void* trace) {
-    strcat((char*) trace, "1");
-    cyield();
-    strcat((char*) trace, "2");
 
     return NULL;
 }
 
 int main() {
     int ret_code;
-    char trace[5] = "";
+    char trace[10] = "";
+    args_t arguments[8] = {
+        {trace, "4", 1},
+        {trace, "7", 2},
+        {trace, "2", 0},
+        {trace, "5", 1},
+        {trace, "8", 2},
+        {trace, "3", 0},
+        {trace, "6", 1},
+        {trace, "9", 2}
+    };
+    int tid[8];
+    int tid_func_2;
 
     start_test("cyield");
 
-    ccreate(func_2, trace, 2);
-    ccreate(func_3, trace, 2);
-    ccreate(func_4, trace, 2);
+    csetprio(0, 0); // Evita preempção por prioridade
 
     strcat(trace, "1");
+
+    for (int i = 0; i < 8; i++) {
+        tid[i] = ccreate(func, &arguments[i], arguments[i].prio);
+    }
 
     ret_code = cyield();
 
     assert("Retorna 0", ret_code == 0);
 
-    assert("A proxima thread da fila eh executada, usando uma politica FIFO", 
-           strcmp(trace, "1234") == 0);
+    assert("A proxima thread da fila eh executada, usando uma politica FIFO "
+           "por prioridade", strcmp(trace, "123456789") == 0);
 
+    for (int i = 0; i < 8; i++) {
+        cjoin(tid[i]);
+    }
 
 
     trace[0] = '\0'; // String vazia
 
-    ccreate(high_prio, trace, 1);
+    tid_func_2 = ccreate(func2, trace, 1);
 
-    strcat(trace, "3");
+    strcat(trace, "1");
+
+    cyield();
+
+    strcat(trace, "2");
 
     assert("Se a thread atual eh a de maior prioridade, e nao ha outra com "
            "mesma prioridade, deve continuar executando", 
-           strcmp(trace, "123") == 0);
+           strcmp(trace, "12") == 0);
+
+    cjoin(tid_func_2);
 }
